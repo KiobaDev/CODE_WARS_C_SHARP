6f77b58c70c78494a8b43fda97a0167ad4d2605e
Jakub Sarecki <62380366+KiobaDev@users.noreply.github.com>
2020-11-04 19:06:06 +0100

    Add files via upload

---
+++ CodeWars/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CodeWars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetMiddle("kubaa"));
+        }
+        public static string GetMiddle(string s)
+        {
+            int x = s.Length / 2;
+            string str = "";
+            if (s.Length % 2 == 0)
+            {
+               
+                str += s[x-1];
+                str += s[x ];
+                return str;
+            }
+            else 
+            str += s[x];
+            return str;
+        }
+    }
+}
