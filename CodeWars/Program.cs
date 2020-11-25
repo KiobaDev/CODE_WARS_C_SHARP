ebfc638d4160239e411d01afe430fedb1a8e7cd6
Jakub Sarecki <62380366+KiobaDev@users.noreply.github.com>
2020-11-25 23:01:14 +0100

    Update Program.cs

--- CodeWars/Program.cs
+++ CodeWars/Program.cs
@@ -8,6 +8,7 @@ namespace CodeWars
         {
             Console.WriteLine(GetMiddle("kubaa"));
         }
+        //funkcja wypisujaca srodkowa litere wyrazu
         public static string GetMiddle(string s)
         {
             int x = s.Length / 2;
@@ -22,10 +23,12 @@ namespace CodeWars
                 str += s[x];
             return str;
         }
+        //funkcja konwertujaca inta do stringa
         public static string NumberToString(int num)
         {
             return num.ToString();
         }
+        // funkcja wypisujaca "Yes" dla parametru word == true w przeciwnym razie wypisuje "No" dla false
         public static string boolToWord(bool word)
         {
             if (word == true)
@@ -35,5 +38,19 @@ namespace CodeWars
             else
                 return "No";
         }
+        //Funkcja sprawdzajaca czy z podanych parametrow mozliwe jest zbudowanie trojakta jesli tak zwraca true
+        public static bool IsTriangle(int a, int b, int c)
+        {
+            var half = (a + b + c) / 2;
+            var square = Math.Sqrt(half * (half - a) * (half - b) * (half - c));
+            if (a+b>c|| a+c>b || b+c>a && square > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+      }
     }
 }
