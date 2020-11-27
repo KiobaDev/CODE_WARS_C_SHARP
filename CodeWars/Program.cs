44841acbb8f1e02b2109076087b79a980ace9b64
Jakub Sarecki <62380366+KiobaDev@users.noreply.github.com>
2020-11-27 23:33:45 +0100

    Update Program.cs

--- CodeWars/Program.cs
+++ CodeWars/Program.cs
@@ -152,4 +152,21 @@ namespace CodeWars
     return sum;
   }
     }
+        //funkcja sortujaca elementy tablicy (wyrazy) po ich dlugosci 
+        public static string[] SortByLength(string[] array)
+        {
+            for(int i=0;i<=array.Length;i++)
+            {
+                for(int j = 0; j < array.Length-1; j++)
+                {
+                    if (array[j].Length-1 > array[j + 1].Length-1)
+                    {
+                        var tmp = array[j + 1];
+                        array[j + 1] = array[j];
+                        array[j] = tmp;
+                    }
+                }
+            }
+            return array;
+        }
 }
